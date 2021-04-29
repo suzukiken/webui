a1f17ff857b3c5749b87bf0c12ed4721decf0da3
suzukiken <suzukiken@gmail.com>
2021-04-29 08:53:56 +0000

    first

---
+++ aws-exports.template.js
@@ -0,0 +1,22 @@
+const awsmobile = {
+    "aws_project_region": "ap-northeast-1",
+    "aws_cognito_identity_pool_id": "${COGNITO_IDPOOL_ID}",
+    "aws_cognito_region": "ap-northeast-1",
+    "aws_user_pools_id": "${COGNITO_USERPOOL_ID}",
+    "aws_user_pools_web_client_id": "${COGNITO_USERPOOL_WEBCLIENT_ID}",
+    "oauth": {
+        "domain": "${COGNITO_USERPOOL_DOMAINNAME}",
+        "scope": [
+            "aws.cognito.signin.user.admin",
+            "email",
+            "openid",
+            "profile"
+        ],
+        "redirectSignIn": "${COGNITO_USERPOOL_SIGNIN_URL}",
+        "redirectSignOut": "${COGNITO_USERPOOL_SIGNOUT_URL}",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS"
+};
+
+export default awsmobile;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+          getUser().then(userData => setUser(userData));
+          console.log('signIn', data);
+          break;
+        case 'signOut':
+          console.log('signOut', data);
+          setUser(null);
+          break;
+        case 'signIn_failure':
+          getUser().then(userData => setUser(userData));
+          console.log('signIn_failure', data);
+          break;
+        default:
+          console.log('default');
+      }
+    });
+    
+    getUser().then(userData => setUser(userData));
+  }, []);
+  
+  useEffect(() => {
+    Auth.currentUserInfo().then((userInfoData) => {
+      setUserInfo(userInfoData);
+      console.log(user);
+    });
+  }, [user]);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className="hero-body">
+        <p className="title">
+          Authenticaton Example Page
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="subtitle">
+          Authorization with Google for AWS Cognito UserPool.
+        </p>
+      </div>
+      
+      <section className="section">
+        <div className="block">
+          User: <strong>{userInfo ? userInfo.attributes.email : 'None'}</strong>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button 
+              className="button is-link"
+              onClick={() => Auth.federatedSignIn()}
+              >Sign In
+            </button>
+          </div>
+          <div className="control">
+            <button 
+              className="button is-link"
+              onClick={() => Auth.signOut()}
+              >Sign Out
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
